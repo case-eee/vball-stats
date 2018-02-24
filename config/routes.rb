5f649a754e2e06d27aa058e0057fc9f84a7cdc21
Casey Ann Cumbow <cumbow8@gmail.com>
2018-02-24 11:05:32 -0500

    Add link between stats and taking stats page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get "/teams/:id/rounds/new" => "rounds#new"
-  get "/teams/:team_id/rounds/:id" => "rounds#show"
+  get "/teams/:team_id/rounds/:id" => "rounds#show", as: :take_stats
   post "/teams/:id/rounds" => "rounds#create"
 
-  get "/rounds/:id/stats" => "rounds#stats"
+  get "/rounds/:id/stats" => "rounds#stats", as: :round_stats
 
   post "/rounds/:round_id/spikes/:player_id" => "spikes#create", as: :round_spike
   post "/rounds/:round_id/passes/:player_id" => "passes#create", as: :round_pass
